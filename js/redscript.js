c00a584c008530bbb62b8c2beb3c0056fb8445d3
gegendepressed <sairaj09pai@gmail.com>
2024-09-08 17:25:55 +0530

    redditapi.html - page for reddit api test

---
+++ js/redscript.js
@@ -0,0 +1,65 @@
+window.onload = function() {
+    document.getElementById("subredditname").value = "";
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    function getSelectedValue() {
+        const selectElement = document.getElementById("options");
+        return selectElement ? selectElement.value : "";
+    }
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+
+        document.getElementById("Joke-Header").textContent = "Loading...";
+        document.getElementById("Joke-Description").textContent = "";
+        document.getElementById("URL").textContent = "";
+        document.getElementById("Date").textContent = "";
+        document.getElementById("copytextbutton").disabled = true;
+        
+        const subreddit = document.getElementById("subredditname").value.trim();
+        const selectedValue = getSelectedValue();
+        const apiUrl = `https://redditapi-sitt.onrender.com/${subreddit}?type=${selectedValue}`;
+        console.log("API URL:", apiUrl);
+
+        fetch(apiUrl)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log("Results fetched.")
+                document.getElementById("Joke-Header").textContent = data.title || "Content not loading, please resubmit again";
+                document.getElementById("Joke-Description").textContent = data.description || "";
+                document.getElementById("URL").textContent = data.url || "";
+                document.getElementById("Date").textContent = data.date ? `Date: ${data.date}` : "";
+                document.getElementById("copytextbutton").disabled = false;
+            })
+            .catch(error => {
+                console.error("Error fetching data: ", error);
+                copytextbutton.disabled = true;
+            });
+    }
+
+    const form = document.getElementById("redditapifetcher");
+    if (form) {
+        form.addEventListener('submit', handleFormSubmit);
+    } else {
+        console.error("Error: Form element not found.");
+    }
+});
+
+function copytext() {
+    const jokeHeader = document.getElementById("Joke-Header").textContent;
+    const jokeDescription = document.getElementById("Joke-Description").textContent;
+    const url = document.getElementById("URL").textContent;
+
+    const combinedText = `${jokeHeader}\n\n${jokeDescription}\nPost Link - "${url}`;
+
+    navigator.clipboard.writeText(combinedText)
+    .then(() => {
+        alert("Copied the text: \n" + combinedText + "\n\n" + "Done using https://sairaj.tabcat.live/redditapi.html") ;
+    })
+    .catch(err => {
+        console.error("Error copying text: ", err);
+    });
+}
